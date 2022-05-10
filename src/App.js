d26f6f54b187e6da85a5e1030a2fed9bf96985d8
vinihgomes <contatoviniciusgomes@gmail.com>
2022-05-10 18:18:37 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Student from "./components/Student";
+
+function App() {
+  return (
+    <div className="App">
+      <Student />
+    </div>
+  );
+}
+
+export default App;
